ba7a2b569954ed06cf0b205a7f5ac5524e813be6
920 <gmldud628@gmail.com>
2020-12-17 20:24:52 +0900

    공통 > 코어 컴포넌트 기능 작업 --- - Api.js 컴포넌트로 변경 완료 #2

--- src/pages/playground/Api.js
+++ src/pages/playground/Api.js
@@ -3,40 +3,6 @@ import types from '@/assets/data/types.json5'
 import Component from '@/core/component'
 import axios from 'axios'
 
-/*
-const setEvent = function (eventType, selector) {
-  // event
-  const targets = [...document.querySelectorAll(selector)]
-  console.log(document, eventType, selector, targets)
-  // const isTarget = targets.includes(target)
-  targets.forEach(item => {
-    item.addEventListener(eventType, (e) => {
-      const target = e.target.tagName === 'li' ? e.target : e.target.closest('li')
-      console.log(target, target.dataset)
-
-      const selectedApi = api[target.dataset.api][target.dataset.index]
-
-      if (selectedApi.loop) {
-        selectedApi.payload.forEach(item => {
-          axios[selectedApi.method](`api${selectedApi.api}`, item)
-            .then((data) => console.log(data))
-            .catch((err) => console.log(err))
-        })
-      } else {
-        axios[selectedApi.method](`api${selectedApi.api}`, selectedApi.payload || null)
-          .then((data) => console.log(data))
-          .catch((err) => console.log(err))
-      }
-    })
-  })
-}
-
-export default async function () {
-  await document.body.appendChild(component())
-  await setEvent('click', '.api')
-}
-*/
-
 export default class Api extends Component {
   data () {
     return {
@@ -60,18 +26,48 @@ export default class Api extends Component {
     }
   }
 
-  flattenTemplate (arr, key) {
-    return `${arr.map((item, idx) => `<li data-name="${item.name}" data-api="${key}" data-index="${idx}" style="margin-bottom: 10px;"><strong>[${item.name}]</strong>method: ${item.method}, api: api${item.api}</li>`).join('')}`
-  }
-
   template () {
     return `
-    <div class="api">
+    <div class="Api">
       <h2>API Test</h2>
       <ul class="api">
         ${Object.keys(this.api).map(key => this.flattenTemplate(this.api[key], key)).join('')}
       </ul>
     </div>
     `
+  }
+
+  mounted () {
+    this.setEvent('click', '.api')
+  }
+
+  flattenTemplate (arr, key) {
+    return `${arr.map((item, idx) => `<li data-name="${item.name}" data-api="${key}" data-index="${idx}" style="margin-bottom: 10px;"><strong>[${item.name}]</strong>method: ${item.method}, api: api${item.api}</li>`).join('')}`
+  }
+  
+  setEvent (eventType, selector) {
+    const targets = [...document.querySelectorAll(selector)]
+    console.log(document, eventType, selector, targets)
+    // const isTarget = targets.includes(target)
+    targets.forEach(item => {
+      item.addEventListener(eventType, (e) => {
+        const target = e.target.tagName === 'li' ? e.target : e.target.closest('li')
+        console.log(target, target.dataset)
+
+        const selectedApi = api[target.dataset.api][target.dataset.index]
+
+        if (selectedApi.loop) {
+          selectedApi.payload.forEach(item => {
+            axios[selectedApi.method](`api${selectedApi.api}`, item)
+              .then((data) => console.log(data))
+              .catch((err) => console.log(err))
+          })
+        } else {
+          axios[selectedApi.method](`api${selectedApi.api}`, selectedApi.payload || null)
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err))
+        }
+      })
+    })
   }
 }
