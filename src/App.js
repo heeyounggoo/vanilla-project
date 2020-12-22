b1206b4bed4e40bcfd279d9d7a006d849feb78bd
920 <gmldud628@gmail.com>
2020-12-22 21:47:38 +0900

    스타일 > 공통 UI 스타일 작업 --- - 스타일 파일 추가 - app.js에 글로벌 스타일 추가 #3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+// TODO 자식 컴포넌트 2개 이상일 경우 랜더 안되는 이슈
+
 import Component from '@/core/component'
+// import Store from '@/core/store/store'
 import SideBar from '@/pages/common/SideBar'
+// import TopBar from '@/pages/common/TopBar'
+// import { install } from '@/mixins/install'
+import '@/assets/style/global.scss'
+
+// install.call(this, Component, {
+//   store: store
+// })
 
 const App = class App extends Component {
   data () {
